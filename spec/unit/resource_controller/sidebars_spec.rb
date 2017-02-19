7092a817c78a98c77e8daae16a52d61516434704
David Rodríguez <deivid.rodriguez@gmail.com>
2017-02-19 10:34:54 -0300

    Extract common setup to a shared context

--- spec/unit/resource_controller/sidebars_spec.rb
+++ spec/unit/resource_controller/sidebars_spec.rb
@@ -3,32 +3,34 @@ require 'rails_helper'
 RSpec.describe ActiveAdmin::ResourceController::Sidebars, type: :controller do
   let(:klass){ Admin::PostsController }
 
-  before do
-    @controller = klass.new
+  shared_context 'with post config' do
+    before do
+      @controller = klass.new
+
+      load_resources { post_config }
+
+      get :index
+    end
   end
 
   context 'without skip_sidebar! before filter' do
-    before do
-      load_resources { ActiveAdmin.register Post }
+    include_context 'with post config' do
+      let(:post_config) { ActiveAdmin.register Post }
     end
 
     it 'does not set @skip_sidebar' do
-      get :index
-
       expect(controller.instance_variable_get(:@skip_sidebar)).to eq nil
     end
   end
 
   context 'with skip_sidebar! before_filter' do
-    before do
-      load_resources do
+    include_context 'with post config' do
+      let(:post_config) do
         ActiveAdmin.register(Post) { before_filter :skip_sidebar! }
       end
     end
 
     it 'works' do
-      get :index
-
       expect(controller.instance_variable_get(:@skip_sidebar)).to eq true
     end
   end
